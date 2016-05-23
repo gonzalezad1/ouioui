f804267a3b755559eed1302eb70639a5c4c5f91d
Morgan <mky.wee@gmail.com>
2016-05-23 18:42:53 +0800

    product photos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,13 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update]
-  before_action :authenticate_admin!, except: [:show]
+  before_action :authenticate_user!, except: [:show]
 
   def index
-    @products = current_admin.products
+    @products = current_user.products
   end
 
   def new
-    @product = current_admin.products.build
+    @product = current_user.products.build
   end
 
   def show
@@ -15,7 +15,7 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = current_admin.products.build(product_params)
+    @product = current_user.products.build(product_params)
     if @product.save
       if params[:images]
         params[:images].each do |img|
@@ -23,8 +23,9 @@ class ProductsController < ApplicationController
         end
       end
       @product_photos = @product.product_photos
-      redirect_to edit_room_path(@product), notice: "Saved..."
+      redirect_to edit_product_path(@product), notice: "Saved..."
     else
+      flash[:alert] = "Please provide all information required."
       render :new
     end
   end
@@ -40,8 +41,9 @@ class ProductsController < ApplicationController
           @product.product_photos.create(image: img)
         end
       end
-      redirect_to edit_room_path(@product), notice: "Updated..."
+      redirect_to edit_product_path(@product), notice: "Updated..."
     else
+      flash[:alert] = "Please provide all information required."
       render :edit
     end
   end
@@ -52,7 +54,7 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:product_type, :category, :size, :product_name, :description, :stock, :price, :code_id, :active)
+    params.require(:product).permit(:product_type, :category, :size, :product_name, :description, :stock, :price)
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,15 @@
 class Product < ActiveRecord::Base
   belongs_to :user
 
-  validates_presence_of :product_type
-  validates_presence_of :category
-  validates_presence_of :size
-  validates_presence_of :product_name, length: {maximum: 50}
-  validates_presence_of :description, length: {maximum: 500}
-  validates_presence_of :stock
-  validates_presence_of :code_id
+  # validates_presence_of :product_type
+  # validates_presence_of :category
+  # validates_presence_of :size
+  # validates_presence_of :product_name, length: {maximum: 50}
+  # validates_presence_of :description, length: {maximum: 500}
+  # validates_presence_of :stock
+  # validates_presence_of :code_id
+  # validates_presence_of :price
+
 
   has_many :product_photos
 
