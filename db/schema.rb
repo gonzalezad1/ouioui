7125db725b7f48c717b25e5632f984a48d010905
Morgan <mky.wee@gmail.com>
2016-05-24 17:49:10 +0800

    bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160524041200) do
-=======
 ActiveRecord::Schema.define(version: 20160524081032) do
->>>>>>> master
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -43,6 +39,13 @@ ActiveRecord::Schema.define(version: 20160524081032) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "galleries", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
@@ -51,6 +54,17 @@ ActiveRecord::Schema.define(version: 20160524081032) do
     t.datetime "updated_at"
     t.string   "token"
   end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "pictures", force: :cascade do |t|
     t.string   "description"
@@ -64,24 +78,6 @@ ActiveRecord::Schema.define(version: 20160524081032) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
-
-  create_table "conversations", force: :cascade do |t|
-    t.integer  "sender_id"
-    t.integer  "recipient_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "conversation_id"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
-  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "product_photos", force: :cascade do |t|
     t.integer  "product_id"
